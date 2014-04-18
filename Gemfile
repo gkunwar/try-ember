a74b7d565ca22f8b96f1dd6ed0671586afb24d85
Ganesh <ganesh@esignature.com.np>
2014-04-18 09:59:47 +0545

    Added ember js and source on the gemfile and configured ember js

--- Gemfile
+++ Gemfile
@@ -46,3 +46,6 @@ end
 
 gem 'bootstrap-sass', '3.1.1.0'
 gem 'rabl', '0.9.3'
+
+gem 'ember-rails'
+gem 'ember-source', '1.4.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require handlebars
+//= require ember
+//= require_self
+//= require blorgh
+
+// for more details see: http://emberjs.com/guides/application/
+window.Blorgh = Ember.Application.create()
